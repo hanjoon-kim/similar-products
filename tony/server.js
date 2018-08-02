323ed9a660ca1fd3118a7b1e3e30e6499b7d2fd3
Hanjoon Kim <hanjoon3@gmail.com>
2018-08-01 20:14:25 -0700

    Implement docker without database integration

---
+++ tony/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3001;
+
+const con = mysql.createConnection({
+  user: 'root',
+  password: 'password',
+  database: 'similarProducts',
+});
+
+app.use(bodyParser.json());
+app.use(express.static('./'));
+
+app.get('/products', function (req, res) {
+  con.connect((err) => {
+    if (err) {
+      console.log('Error connecting to MySql');
+      return;
+    }
+    console.log('Connected to database');
+  })
+
+  var query = 'SELECT * FROM products';
+
+  con.query(query, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Query successful');
+      res.send(result);
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
